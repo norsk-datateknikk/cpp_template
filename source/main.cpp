003652e0e2c555eb48bc7e5cf8f51032e0261421
Erik Buer <erik@localhost.localdomain>
2021-04-08 21:53:57 +0200

    Added main file

---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+
+//--------------------------------------------------------------//
+// Norsk Datateknikk AS 2021                                    //
+//--------------------------------------------------------------//
+// This file is subject to the terms and conditions defined in  //
+// file 'LICENSE', which is part of this source code package.   //
+//--------------------------------------------------------------//
+
+int main (int argc, char *argv[])
+{
+    return 0;
+}
